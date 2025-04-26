721254a935c27de3cdb61644ee13893636e95bf8
Harsh panda <122609133+pandaharsh@users.noreply.github.com>
2025-04-26 11:45:46 +0530

    Time: 0 ms (100%), Space: 9.6 MB (93.89%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,12 @@
+int removeElement(int* nums, int numsSize ,int val){
+    int i, count=0;
+    for(i=0; i<numsSize; i++) {
+        if(nums[i]==val) {
+            count++;
+        }
+        else {
+            nums[i-count]= nums[i];
+        }
+    }
+    return (numsSize-count);
+}
